09f73f9c4c41b1d4e14bda0662aad45e4494a286
duck12212 <duck12212@gmail.com>
2014-12-19 17:57:02 +0800

    Create consumer.c

---
+++ Lab10_Semaphores/consumer.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+int main()
+{
+	const int size = 4096;
+	const char *name = "OS";
+	int shm_fd;
+	void *ptr;
+	shm_fd = shm_open(name,O_RDONLY,0666);
+	ptr = mmap(0,size,PROT_READ,MAP_SHARED,shm_fd,0);
+	printf("%s",(char *)ptr);
+	printf("\n");
+	shm_unlink(name);
+	return 0;
+}
